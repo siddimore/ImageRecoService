999c11bc46830ce7f60029ab23486ce7a11dd1d2
Siddharth More <siddharth@m-87.com>
2018-06-20 08:01:52 -0700

    Image Classification Service Using PreTrained Models

---
+++ Readme.md
@@ -0,0 +1,39 @@
+Service To Do Basic Image Classification using keras and flask
+
+a. PreTrained Model Used like ResNet50 and InceptionV3
+b. Flask Service To Wrap the Model and load it
+c. Predict API to give image and get results
+
+Sample Test
+1. Git Clone Repo
+2. In Terminal Navigate to where Project is cloned
+3. Run Service: python imageclassifierservice.py
+4. Open another terminal window
+5. curl -X POST -F image=@dog.jpg 'http://localhost:5000/predict'
+
+Result For Dog Image:
+{
+  "predictions": [
+    {
+      "label": "beagle",
+      "probablity": 0.9830568432807922
+    },
+    {
+      "label": "pot",
+      "probablity": 0.004184032324701548
+    },
+    {
+      "label": "Walker_hound",
+      "probablity": 0.0036015023943036795
+    },
+    {
+      "label": "bluetick",
+      "probablity": 0.0016072390135377645
+    },
+    {
+      "label": "Brittany_spaniel",
+      "probablity": 0.0010771512752398849
+    }
+  ],
+  "success": true
+}
---
+++ imageclassifierservice.py
@@ -0,0 +1,72 @@
+from keras.applications import InceptionV3,ResNet50
+from keras.preprocessing.image import img_to_array
+from keras.applications import imagenet_utils
+from PIL import Image
+import numpy as np
+import flask
+import io
+
+
+# Initialize Flask application
+app = flask.Flask(__name__)
+model = None
+
+
+def load_model():
+    # Load Pretrained Model
+    global model
+    # model = InceptionV3(weights = "imagenet")
+    model = ResNet50(weights = "imagenet")
+
+
+def prepare_image(image, target):
+    if image.mode != "RGB":
+        image = image.convert("RGB")
+
+    # resize input image and preprocess
+    image = image.resize(target)
+    image = img_to_array(image)
+    image = np.expand_dims(image, axis=0)
+    image = imagenet_utils.preprocess_input(image)
+
+    return image
+
+@app.route("/predict", methods=["POST"])
+def predict():
+
+    # Init the data dictionaru that will be returned to the view
+    data = {"success": False}
+
+    if flask.request.method == "POST":
+        if flask.request.files.get("image"):
+
+            # Read image
+            image = flask.request.files["image"].read()
+            image = Image.open(io.BytesIO(image))
+
+            image = prepare_image(image, target=(224,224))
+
+            # classify image and init list of predictions to returned
+            preds = model.predict(image)
+            results = imagenet_utils.decode_predictions(preds)
+
+            data["predictions"] = []
+
+            # Loop over results
+            for(imagenetID, label, prob) in results[0]:
+                r = {"label":label, "probablity":float(prob)}
+                data["predictions"].append(r)
+
+            data["success"] = True
+
+    return flask.jsonify(data)
+
+
+
+# if this is the main thread of execution first load the model and
+# then start the server
+if __name__ == "__main__":
+    print(("* Loading Keras model and Flask starting server..."
+        "please wait until server has fully started"))
+    load_model()
+    app.run()
